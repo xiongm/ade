38e06fdee1c9eaf9e4a7873118626201c4b13571
xiongm <xiongm@gmail.com>
2019-05-26 04:52:50 +0000

    update

--- archive/matching_engine/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/mxiong/projects/ade/archive/matching_engine
-# Build directory: /home/mxiong/projects/ade/archive/matching_engine
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(main_ut "/home/mxiong/projects/ade/archive/matching_engine/main_ut")
