e7e29c44250eb5451bd6ef89677cfdd934cfbc21
xiongm <xiongm@gmail.com>
2019-05-26 04:33:04 +0000

    r #0000: update

--- archive/matching_engine/CTestTestfile.cmake
+++ archive/matching_engine/CTestTestfile.cmake
@@ -1,7 +1,7 @@
 # CMake generated Testfile for 
-# Source directory: /home/mxiong/cboe/matching_engine
-# Build directory: /home/mxiong/cboe/matching_engine
+# Source directory: /home/mxiong/projects/ade/archive/matching_engine
+# Build directory: /home/mxiong/projects/ade/archive/matching_engine
 # 
 # This file includes the relevant testing commands required for 
 # testing this directory and lists subdirectories to be tested as well.
-add_test(mytest "/home/mxiong/cboe/matching_engine/main_ut")
+add_test(main_ut "/home/mxiong/projects/ade/archive/matching_engine/main_ut")
--- archive/matching_engine/cmake_install.cmake
+++ archive/matching_engine/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /home/mxiong/cboe/matching_engine
+# Install script for directory: /home/mxiong/projects/ade/archive/matching_engine
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/home/mxiong/cboe/matching_engine/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/home/mxiong/projects/ade/archive/matching_engine/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
