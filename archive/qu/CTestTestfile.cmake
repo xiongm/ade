dd1b83324d6764e22303afebaf4189bfd81d39a4
xiongm <xiongm@gmail.com>
2019-05-26 04:57:41 +0000

    update

--- archive/qu/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/mxiong/projects/ade/archive/quantlab
-# Build directory: /home/mxiong/projects/ade/archive/quantlab
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(main_ut "/home/mxiong/projects/ade/archive/quantlab/main_ut")
