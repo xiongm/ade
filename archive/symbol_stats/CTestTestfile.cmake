88799316af90c568c54009103c9d56d172df928e
xiongm <xiongm@gmail.com>
2019-05-26 04:10:01 +0000

    r #0000: update

--- archive/symbol_stats/CTestTestfile.cmake
+++ archive/symbol_stats/CTestTestfile.cmake
@@ -1,7 +1,7 @@
 # CMake generated Testfile for 
-# Source directory: /home/mxiong/interviews/symbol_stats
-# Build directory: /home/mxiong/interviews/symbol_stats
+# Source directory: /home/mxiong/projects/ade/archive/symbol_stats
+# Build directory: /home/mxiong/projects/ade/archive/symbol_stats
 # 
 # This file includes the relevant testing commands required for 
 # testing this directory and lists subdirectories to be tested as well.
-add_test(mytest "/home/mxiong/interviews/symbol_stats/main_ut")
+add_test(main_ut "/home/mxiong/projects/ade/archive/symbol_stats/main_ut")
--- archive/symbol_stats/cmake_install.cmake
+++ archive/symbol_stats/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /home/mxiong/interviews/symbol_stats
+# Install script for directory: /home/mxiong/projects/ade/archive/symbol_stats
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/home/mxiong/interviews/symbol_stats/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/home/mxiong/projects/ade/archive/symbol_stats/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
