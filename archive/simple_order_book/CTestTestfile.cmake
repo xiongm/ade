347912b5f459fa6f9a4f2adb5851ca0e57108476
xiongm <xiongm@gmail.com>
2019-05-26 04:55:10 +0000

    update

--- archive/simple_order_book/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/mxiong/projects/ade/archive/simple_order_book
-# Build directory: /home/mxiong/projects/ade/archive/simple_order_book
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(main_ut "/home/mxiong/projects/ade/archive/simple_order_book/main_ut")
